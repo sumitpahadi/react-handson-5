83f3175d0f2dddd2a1b0f249870d8241465421fe
sumitpahadi <sumitrawat1102000@gmail.com>
2023-08-14 18:46:04 +0530

    last commit

--- src/Component/PureCounter.js
+++ src/Component/PureCounter.js
@@ -4,20 +4,19 @@ import "./Style.css";
 const PureCounter = React.memo(({ count, onIncrement, onDecrement }) => {
   console.log("PureCounter rendered"); // This will only be called when props change
   return (
-    <div className="counter">
-        
-
-      <div className="button">
-        <button onClick={onIncrement}>Increment</button>
-      </div>
-      <div className="p">
-        <p>Count: {count}</p>
-      </div>
-      <div className="button">
-        <button onClick={onDecrement}>Decrement</button>
-      
+    
+      <div className="counter">
+        <div className="button">
+          <button onClick={onIncrement}>Increment</button>
         </div>
-    </div>
+        <div className="p">
+          <p>Count: {count}</p>
+        </div>
+        <div className="button">
+          <button onClick={onDecrement}>Decrement</button>
+        </div>
+      </div>
+    
   );
 });
 
